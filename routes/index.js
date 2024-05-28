2a79c439171ff1550afc78fee914a6c47608810b
Michael Reyes <maaaik.dev@gmail.com>
2024-05-28 16:38:50 -0500

    upload file landing page

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
-const indexController = require("../controllers/index")
+const multer = require('multer');
+const path = require('path');
+const indexController = require("../controllers/index");
 
 router.get('/', indexController.index);
-
 router.get('/products', indexController.listOfProducts);
-
 router.post('/new-product', indexController.newProduct);
 
 /* Hispam Page */
-
 router.get('/save-html', indexController.saveHtmlGet);
-
 router.post('/save-html', indexController.saveHtmlPost);
-
 router.get('/get-html', indexController.getHtml);
-
 router.delete('/remove-html/:id', indexController.removeHtml);
+
+// Configuración de multer para guardar las imágenes en una carpeta 'uploads'
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+
+//router.use('/uploads', express.static('uploads')); // Enrutador estático para la carpeta 'uploads'
+
+const upload = multer({ storage });
+router.post('/upload', upload.array('files[]'), indexController.uploadImages);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,50 +1,3 @@
-// const express = require('express');
-// const cors = require('cors'); // Importa el paquete cors
-// const app = express();
-// const port = 3001;
-
-// const fs = require('fs');
-
-// app.use(cors()); // Usa el middleware cors para permitir solicitudes CORS
-
-// app.use(express.json());
-
-// // Ruta para la raíz, puede devolver un mensaje simple
-// app.get('/', (req, res) => {
-//     res.send('¡Servidor Express en funcionamiento!');
-// });
-
-// // Rutas para guardar y recuperar el HTML
-// app.post("/save-html", (req, res) => {
-//     debugger
-//     console.log("REQ, RES", [req, res])
-//     // Aquí puedes guardar el HTML en un archivo o base de datos local
-//     const { html } = req.body;
-
-//   // Genera un nombre de archivo único basado en la fecha actual
-//   const nombreArchivo = `archivo-${Date.now()}.html`;
-
-//   // Escribe el HTML en un archivo en el directorio especificado
-//   fs.writeFile(rutaDirectorioHTML + nombreArchivo, html, (err) => {
-//     if (err) {
-//       console.error('Error al guardar el archivo HTML', err);
-//       res.status(500).json({ error: 'Error al guardar el archivo HTML' });
-//     } else {
-//       console.log('Archivo HTML guardado correctamente');
-//       res.status(200).json({ message: 'Archivo HTML guardado correctamente', fileName: nombreArchivo });
-//     }
-//   });
-// });
-
-// app.get('/get-html', (req, res) => {
-//     // Aquí debes recuperar el HTML guardado y enviarlo como respuesta
-//     res.status(200).sendFile(__dirname + '/ruta/al/archivo.html')
-// });
-
-// app.listen(port, () => {    
-//     console.log(`Servidor backend iniciado en http://localhost:${port}`)
-// })
-
 // server.js
 const express = require('express');
 const cors = require('cors'); // Importa el paquete cors
@@ -74,7 +27,8 @@ app.use(cors());
 app.use(routes)
 
 //Static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', routes); // Usa las rutas definidas primero
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Start the server
 app.listen(port, () => {
